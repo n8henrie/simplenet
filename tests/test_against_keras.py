dfaed89a7c987b077f6fa7f7979cc7f4b319b30d
Nathan Henrie <nate@n8henrie.com>
2017-11-03 08:04:25 -0600

    No f-strings on 3.5

--- tests/test_against_keras.py
+++ tests/test_against_keras.py
@@ -25,7 +25,7 @@ nn = sn.SimpleNet(
     )
 
 keras.backend.set_floatx(dtype)
-print(f'keras epsilon: {keras.backend.epsilon()}')
+print('keras epsilon: {}'.format(keras.backend.epsilon()))
 
 inputs = Input(shape=(len(data[0]),), dtype=dtype)
 x = Dense(9, activation='sigmoid')(inputs)
