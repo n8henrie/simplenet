cfa4ff62e21f76d2a853528af401d45dce32a8e3
Nathan Henrie <nate@n8henrie.com>
2022-06-19 13:50:20 -0600

    Expand tests, make compatible with modern tf and keras

--- tests/test_against_keras.py
+++ tests/test_against_keras.py
@@ -1,65 +1,101 @@
 """Compare an update against an identical network in Keras."""
 
-import keras
+from copy import deepcopy
+
 import numpy as np
-from keras.layers import Dense, Input
-from keras.models import Model
-from keras.optimizers import SGD
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
 
 import simplenet as sn
 
-data = [[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1], [1, 1, 0]]
+dtype = "float64"
+keras.backend.set_floatx(dtype)
+
+inputs = [[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1], [1, 1, 0]]
 targets = [[0, 1], [1, 0], [1, 0], [0, 1], [1, 0]]
 
+shapes = [9, 7, 5, 3]
 learning_rate = 0.1
-dtype = 'float64'
+
+input_size = (None, len(inputs[0]))
+output_size = (None, len(targets[0]))
 
 nn = sn.SimpleNet(
-    input_shape=(None, len(data[0])), output_shape=(None, len(targets[0])),
-    hidden_layer_sizes=[9, 7, 5, 3],
+    input_shape=input_size,
+    output_shape=output_size,
+    hidden_layer_sizes=shapes,
     learning_rate=learning_rate,
     activation_function=sn.sigmoid,
     output_activation=sn.softmax,
     loss_function=sn.cross_entropy,
     dtype=dtype,
-    )
+)
 
-keras.backend.set_floatx(dtype)
+initial_weights, initial_biases = deepcopy(nn.weights), deepcopy(nn.biases)
 
-inputs = Input(shape=(len(data[0]),), dtype=dtype)
-x = Dense(9, activation='sigmoid')(inputs)
-x = Dense(7, activation='sigmoid')(x)
-x = Dense(5, activation='sigmoid')(x)
-x = Dense(3, activation='sigmoid')(x)
-outputs = Dense(len(targets[0]), activation='softmax')(x)
+keras_inputs = Input(shape=(len(inputs[0]),))
 
-model = Model(inputs=inputs, outputs=outputs)
-optimizer = SGD(lr=learning_rate)
+x = keras_inputs
+for shape in [9, 7, 5, 3]:
+    x = Dense(shape, activation="sigmoid")(x)
+outputs = Dense(len(targets[0]), activation=None)(x)
+
+model = Model(inputs=keras_inputs, outputs=outputs)
+optimizer = SGD(learning_rate=learning_rate)
 model.compile(
-        optimizer=optimizer,
-        loss='categorical_crossentropy',
+    optimizer=optimizer, loss=CategoricalCrossentropy(from_logits=True)
+)
+
+model.set_weights(
+    [
+        layer
+        for weight, bias in zip(
+            deepcopy(initial_weights), deepcopy(initial_biases)
         )
+        for layer in (weight, bias.reshape(-1))
+    ]
+)
 
-model.set_weights([layer for weight, bias in zip(nn.weights, nn.biases)
-                   for layer in (weight, bias.reshape(-1))])
+epochs = 10
 
-nn.learn(inputs=data, targets=targets)
-history = model.fit(data, targets, epochs=1, verbose=False)
+for _ in range(epochs):
+    nn.learn(inputs=inputs, targets=targets)
+
+history = model.fit(
+    np.array(inputs, dtype=dtype),
+    np.array(targets, dtype=dtype),
+    epochs=10,
+    verbose=False,
+)
+
+pairs = {
+    "weights": (model.get_weights()[::2], nn.weights),
+    "biases": (model.get_weights()[1::2], nn.biases),
+}
+
+
+def test_learned_something() -> None:
+    """Ensure that the weights and biases are changing."""
+    for idx, (keras_data, nn_data) in enumerate(zip(*pairs["weights"])):
+        assert not np.allclose(nn_data, initial_weights[idx])
+        assert not np.allclose(keras_data, initial_weights[idx])
+    for idx, (keras_data, nn_data) in enumerate(zip(*pairs["biases"])):
+        assert not np.allclose(nn_data, initial_biases[idx])
+        assert not np.allclose(keras_data, initial_biases[idx])
 
 
 def test_weights_and_biases() -> None:
-    """Ensure SimpleNet weights and biases match keras after 1 epoch."""
+    """Ensure SimpleNet weights and biases match keras."""
     # `model.get_weights()` returns a list of alternating weights and biases
-    pairs = {
-        'weights': (model.get_weights()[::2], nn.weights),
-        'biases': (model.get_weights()[1::2], nn.biases),
-    }
 
-    for k, pair in pairs.items():
-        for idx, (keras_data, nn_data) in enumerate(zip(*pair)):
+    for pair in pairs.values():
+        for keras_data, nn_data in zip(*pair):
             assert np.allclose(keras_data, nn_data)
 
 
 def test_err() -> None:
-    """Ensure SimpleNet error matches keras after 1 epoch."""
-    assert np.isclose(nn.err, history.history['loss'][0])
+    """Ensure SimpleNet error matches keras."""
+    assert np.isclose(nn.err, history.history["loss"][-1])
--- tests/test_simplenet.py
+++ tests/test_simplenet.py
@@ -1,5 +1,7 @@
 """test_simplenet.py :: Tests for `simplenet` module."""
 
+import tempfile
+
 import numpy as np
 
 import simplenet as sn
@@ -20,9 +22,9 @@ def test_validate_neg_log_likelihood() -> None:
         activation_function=sn.sigmoid,
         output_activation=sn.sigmoid,
         loss_function=sn.neg_log_likelihood,
-        dtype=np.float64,
+        dtype="float64",
         seed=42,
-        )
+    )
 
     assert nn.validate(inputs=inputs, targets=targets, epsilon=1e-7)
 
@@ -42,8 +44,56 @@ def test_validate_cross_entropy() -> None:
         activation_function=sn.sigmoid,
         output_activation=sn.sigmoid,
         loss_function=sn.neg_log_likelihood,
-        dtype=np.float64,
+        dtype="float64",
         seed=42,
-        )
+    )
 
     assert nn.validate(inputs=inputs, targets=targets, epsilon=1e-7)
+
+
+def test_import_export() -> None:
+    """Test that I can export and re-import weights."""
+    dtype = "float64"
+    inputs = [[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1], [1, 1, 0]]
+    targets = [[0, 1], [1, 0], [1, 0], [0, 1], [1, 0]]
+
+    shapes = [9, 7, 5, 3]
+    learning_rate = 0.1
+
+    input_size = (None, len(inputs[0]))
+    output_size = (None, len(targets[0]))
+
+    nn = sn.SimpleNet(
+        input_shape=input_size,
+        output_shape=output_size,
+        hidden_layer_sizes=shapes,
+        learning_rate=learning_rate,
+        activation_function=sn.sigmoid,
+        output_activation=sn.softmax,
+        loss_function=sn.cross_entropy,
+        dtype=dtype,
+    )
+
+    nn2 = sn.SimpleNet(
+        input_shape=input_size,
+        output_shape=output_size,
+        hidden_layer_sizes=shapes,
+        learning_rate=learning_rate,
+        activation_function=sn.sigmoid,
+        output_activation=sn.softmax,
+        loss_function=sn.cross_entropy,
+        dtype=dtype,
+    )
+
+    for idx, weight in enumerate(nn.weights):
+        assert not np.allclose(weight, nn2.weights[idx])
+
+    with tempfile.NamedTemporaryFile(suffix=".npz") as f:
+        nn.export_model(f.name)
+        nn2.import_model(f.name)
+
+    for idx, weight in enumerate(nn.weights):
+        assert np.allclose(weight, nn2.weights[idx])
+
+    for idx, bias in enumerate(nn.biases):
+        assert np.allclose(bias, nn2.biases[idx])
